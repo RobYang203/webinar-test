5abea899d646db195fe1853f1c7667a2ca36860b
RobYang203 <101m06014@stud.sju.edu.tw>
2021-11-21 23:00:26 +0800

    建立 webinar ui

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
-import { FaAngleDown, FaChevronDown } from 'react-icons/fa';
+import { FaAngleDown, FaChevronDown, FaChevronRight } from 'react-icons/fa';
 
 const useStyles = createUseStyles(() => {
   return {
@@ -71,46 +71,136 @@ const useStyles = createUseStyles(() => {
       },
     },
     outlinedButton: {
+      cursor: 'pointer',
       flexBasis: 100,
-      flexShrink: 0,
+      flexShrink: 1,
       flexGrow: 0,
       height: 43,
-      background: '#fff',
+      background: '#ffffff00',
       border: '1px solid #152B45',
+      color: '#152B45',
       borderRadius: 4,
       userSelect: 'none',
       '&:hover': {
-        background: '#c0c0c0',
+        background: '#efefef',
+        borderColor: '#013B81',
       },
     },
     containedButton: {
+      cursor: 'pointer',
       flexBasis: 100,
-      flexShrink: 0,
+      flexShrink: 1,
       flexGrow: 0,
       height: 43,
       background: '#013B81',
       borderRadius: 4,
       color: '#fff',
       userSelect: 'none',
+      border: 0,
       '&:hover': {
         background: '#052f62',
       },
     },
-    intro:{
-        padding: `57px 284px 0 282px`,
-        textAlign: 'center',
-        '& > h2':{
-            fontSize: 28,
-            color: '#01254F',
-            fontFamily: 'SF Pro Display',
-            marginBottom: 20
-        },
-        '& > p':{
-            fontSize: 16,
-            color: '#000000A6',
-            fontFamily: 'SF Pro Display'
-        }
-    }
+    intro: {
+      padding: `57px 284px 72px 282px`,
+      letterSpacing: '-0.7px',
+      textAlign: 'center',
+      '& > h2': {
+        fontSize: 28,
+        color: '#01254F',
+        fontFamily: 'SF Pro Display',
+        marginBottom: 20,
+      },
+      '& > p': {
+        fontSize: 16,
+        color: '#000000A6',
+        fontFamily: 'SF Pro Display',
+        lineHeight: '26px',
+      },
+    },
+    webinars: {
+      background: '#F2F2F2',
+      height: 780,
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      flexWrap: 'wrap',
+      padding: `57px 92px 72px 94px`,
+    },
+    webinarItem: {
+      flexBasis: 380,
+      flexGrow: 0,
+      height: 300,
+      background: '#FFF',
+      border: '1px solid #D6D6D6',
+      borderRadius: 4,
+      boxShadow: '1px 2px 6px #DBDBDB80'
+    },
+    webinarItemContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      flexWrap: 'nowrap',
+      height: '100%',
+      padding: '0 20px',
+    },
+    webinarItemHeader: {
+      color: '#01254F',
+    },
+    webinarItemTitle: {
+      margin: '20px 0',
+      color: '#01254F',
+    },
+    webinarItemSubtitle: {
+      margin: 0,
+    },
+    webinarItemBody: {
+      flex: 1,
+      fontSize: 14,
+      color: '#000000A6',
+      fontFamily: 'SF Pro Display',
+    },
+    webinarItemContent: {
+      margin: '20px 0',
+    },
+    webinarItemTime: {},
+    webinarItemFooter: {
+      margin: '20px 0',
+    },
+    webinarItemButtonGroup:{
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    webinarItemButton: {
+      padding: 0,
+      background: '#ffffff00',
+      border: 0,
+      color: '#6BB718',
+      cursor: 'pointer',
+      fontSize: 16,
+      '&:hover': {
+        background: '#efefef',
+      },
+    },
+    webinarItemIconButton: {
+      padding: '0 8px 0 4px',
+      background: '#ffffff00',
+      border: '1px solid #6BB718',
+      color: '#6BB718',
+      cursor: 'pointer',
+      fontSize: 16,
+      width: 20,
+      height: 20,
+      textAlign: 'center',
+      borderRadius: 10,
+      lineHeight: '5px',
+      '&:hover': {
+        background: '#efefef',
+      },
+    },
   };
 });
 
@@ -171,13 +261,43 @@ function MainPage() {
       <main>
         <section className={classes.intro}>
           <h2>Forex Webinars</h2>
-          <div>
+          <p>
             Whether you are new to foreign exchange trading or already have some
             market experience, we believe that a solid FX trading education is
             vital to your success as a trader.
-          </div>
+          </p>
         </section>
-        <section></section>
+        <section className={classes.webinars}>
+          <div className={classes.webinarItem}>
+            <div className={classes.webinarItemContainer}>
+              <div className={classes.webinarItemHeader}>
+                <h5 className={classes.webinarItemTitle}>31/10/2019</h5>
+                <h4 className={classes.webinarItemSubtitle}>
+                  A structured approach to deciphering FX & Gold sentiment
+                </h4>
+              </div>
+              <div className={classes.webinarItemBody}>
+                <p className={classes.webinarItemContent}>
+                  Market scan across FX & Gold to determine sentiment with
+                  accuracy.
+                </p>
+                <p className={classes.webinarItemTime}>7pm-8:30pm EST</p>
+              </div>
+              <div className={classes.webinarItemFooter}>
+                <div className={classes.webinarItemButtonGroup}>
+                  <button className={classes.webinarItemButton}>
+                    Register Now
+                  </button>
+                  <button className={classes.webinarItemIconButton}>
+                    <FaChevronRight size={12} />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={classes.webinarItem}></div>
+          <div className={classes.webinarItem}></div>
+        </section>
         <section></section>
       </main>
     </div>
