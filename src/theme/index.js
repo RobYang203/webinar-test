0fbf68795ca2132540064aa7ae0417271ea93ad4
RobYang203 <101m06014@stud.sju.edu.tw>
2021-11-22 04:47:36 +0800

    調整 theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -3,9 +3,44 @@ import { jss } from 'react-jss';
 
 const theme = {
   ...normalize,
+  palette:{
+    primary:{
+      light: '#1454a1',
+      main: '#013B81',
+      contrastText: '#fff',
+      dark: '#052f62',
+    },
+    secondary:{
+      light: '#fff',
+      main: '#e3e3e3',
+      contrastText: '#4A4A4A',
+      dark: '#d5d5d5'
+    },
+    thirdly:{
+      light: '#fff',
+      main: '#e3e3e3',
+      contrastText: '#152B45',
+      dark: '#d5d5d5'
+    },
+    info:{
+      light: '#81c784',
+      main: '#6BB718',
+      dark: '#388e3c',
+      contrastText: '#fff',
+    },
+    error:{
+      light: '#e57373',
+      main: '#f44336',
+      dark: '#d32f2f',
+      contrastText: '#fff',
+    },
+    textPrimary: '#01254F',
+    textSecondary: '#000000A6',
+    textThirdly: '#4A4A4A'
+  }
 };
 
 export default function configureTheme() {
-  jss.createStyleSheet(theme).attach();
+  jss.createStyleSheet(normalize).attach();
   return theme;
 };
