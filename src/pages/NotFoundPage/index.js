b2a44e65417c8383fd5256d00833f5cf9ba5bd15
RobYang203 <101m06014@stud.sju.edu.tw>
2021-11-25 01:08:58 +0800

    建立 PrivateRoute

--- src/pages/NotFoundPage/index.js
+++ src/pages/NotFoundPage/index.js
@@ -1,7 +1,28 @@
+import Introduction from 'components/Introduction';
 import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles(({ commonStyles }) => {
+  return {
+    root: {
+      ...commonStyles.pageRootStyle,
+    },
+  };
+});
+
 
 function NotFoundPage() {
-  return <div>404</div>;
+  const classes = useStyles();
+
+
+  return (
+    <div className={classes.root}>
+      <Introduction
+        title={404}
+        content={'Page not found'}
+      />
+    </div>
+  );
 }
 
 export default NotFoundPage;
